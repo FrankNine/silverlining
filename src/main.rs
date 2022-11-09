38c829cbd4e40a7db35a6b74a3e9ccaa72330bd0
Chun-Fu Chao <aaefiikmnnnr@gmail.com>
2022-11-10 01:04:35 +0800

    Sheet UI

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,40 @@ mod support;
 
 fn main() {
     let system = support::init(file!());
-    let mut value = 0;
-    let choices = ["test test this is 1", "test test this is 2"];
-    system.main_loop(move |_, ui| {
-        Window::new("Hello world")
-            .size([300.0, 110.0], Condition::FirstUseEver)
-            .build(ui, || {
-                ui.text_wrapped("Hello world!");
-                ui.text_wrapped("こんにちは世界！");
-                if ui.button(choices[value]) {
-                    value += 1;
-                    value %= 2;
-                }
 
-                ui.button("This...is...imgui-rs!");
-                ui.separator();
-                let mouse_pos = ui.io().mouse_pos;
-                ui.text(format!(
-                    "Mouse Position: ({:.1},{:.1})",
-                    mouse_pos[0], mouse_pos[1]
-                ));
-            });
+    const COLUMN_COUNT: usize = 10;
+    const ROW_COUNT: usize = 8;
+    let mut state: [[String; ROW_COUNT]; COLUMN_COUNT] = Default::default();
+
+    system.main_loop(move |_, ui| {
         Window::new("Table")
             .size([300.0, 110.0], Condition::FirstUseEver)
             .build(ui, || {
-                let t = ui.begin_table("Table", 3);
-                for row in 0..4
-                {
+                let t = ui.begin_table("Table", COLUMN_COUNT);
+                for row in 0..ROW_COUNT {
                     ui.table_next_row();
-                    for column in 0..3
-                    {
+                    for column in 0..COLUMN_COUNT {
                         ui.table_set_column_index(column);
-                        ui.text(format!("Row {} Column {}", row, column));
+                        if row == 0 && column != 0 {
+                            ui.text(format!("{}", column));
+                        }
+
+                        if column == 0 && row != 0 {
+                            ui.text(format!(
+                                "{}",
+                                char::from_u32((b'A' + row as u8 - 1) as u32).unwrap()
+                            ));
+                        }
+
+                        if row == 0 && column == 0 {
+                            continue;
+                        }
+
+                        if row != 0 && column != 0 {
+                            let id = ui.push_id(&format!("r{}c{}", row, column));
+                            ui.input_text("", &mut state[column][row]).build();
+                            id.pop();
+                        }
                     }
                 }
                 t.unwrap().end();
